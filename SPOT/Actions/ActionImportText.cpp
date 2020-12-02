54a17f59a0e592b68b513ea5d6634fc0e2c30d0c
Ezzat Esam <s-ezzat.eisawy@zewailcity.edu.eg>
2020-12-02 17:22:28 +0200

    trying to read files

--- SPOT/Actions/ActionImportText.cpp
+++ SPOT/Actions/ActionImportText.cpp
@@ -1,1 +1,32 @@
 #include "ActionImportText.h"
+#include <iostream>
+#include <fstream>
+#include "..\Registrar.h"
+
+ActionImportText::ActionImportText(Registrar* p) : Action(p)
+{
+}
+bool ActionImportText::Execute()
+{
+	GUI* pGUI = pReg->getGUI();
+	pGUI->PrintMsg("Enter the name or the path of the study plan file :");
+	string file_name = pGUI->GetSrting();
+	vector<string> Lines;
+	string Line;
+	ifstream Myfile(file_name);
+	if (!Myfile.is_open())
+		return false;
+	else
+
+		while (getline(Myfile,Line))
+		{
+			Lines.push_back(Line);
+		}
+		Myfile.close();
+
+
+}
+ActionImportText::~ActionImportText()
+{
+
+}
--- SPOT/Actions/ActionImportText.h
+++ SPOT/Actions/ActionImportText.h
@@ -2,12 +2,14 @@
 #include "Action.h"
 #include <vector>
 #include <string>
+using namespace std;
 class ActionImportText    :public Action
 {
 public:
 	ActionImportText(Registrar*);
 	bool virtual Execute();
+
 	virtual ~ActionImportText();
-	vector<string> 
+	
 };
 
