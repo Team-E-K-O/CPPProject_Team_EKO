b3e5f5f3faab73494de8c5f474bdd1fb408645ac
Kareem Sinbel <kareem.sinbel@gmail.com>
2020-12-02 15:54:37 +0200

    Move Course BUG fix

--- SPOT/Actions/ActionMove.cpp
+++ SPOT/Actions/ActionMove.cpp
@@ -30,17 +30,22 @@ bool ActionMove::Execute()
 		pp->DetYearSem(gInfo, year, sem);
 		Course* pc = pp->ReturnCoursePointer(gInfo, year, sem);
 		pc->DrawMe(pGUI);
-		ActionData actData2 = pGUI->GetUserAction("Choose where you want to move it ...");
-		x2 = actData2.x;
-		y2 = actData2.y;
-		graphicsInfo gInfo2{ x2, y2 };
-		pp->DetYearSem(gInfo2, year2, sem2);
-		pc->setSelected(false);
-		pp->AddCourse(pc, year2, sem2);
+		while (true)
+		{
+			ActionData actData2 = pGUI->GetUserAction("Choose where you want to move it ...");
+			if (actData2.actType == DRAW_AREA)
+			{
+				x2 = actData2.x;
+				y2 = actData2.y;
+				graphicsInfo gInfo2{ x2, y2 };
+				pp->DetYearSem(gInfo2, year2, sem2);
+				pc->setSelected(false);
+				pp->AddCourse(pc, year2, sem2);
 
-		pp->DeleteStudyPlan(gInfo, year, sem);
-
-		
+				pp->DeleteStudyPlan(gInfo, year, sem);
+				break;
+			}
+		}
 
 		return true;
 	}
