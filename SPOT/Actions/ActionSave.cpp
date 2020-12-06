5859db180803592c2237c99eb4b04a814fc388f4
omarstark <ogamal4731@gmail.com>
2020-12-06 22:42:08 +0200

    Done b2a

--- SPOT/Actions/ActionSave.cpp
+++ SPOT/Actions/ActionSave.cpp
@@ -20,38 +20,6 @@ bool ActionSave::Execute()
 	string filename = pGUI->GetSrting();
 	StudyPlan* pp = pReg->getStudyPlan();
 	ofstream myfile;
-<<<<<<< HEAD
-	myfile.open(filename + ".txt");
-
-	int year;
-	SEMESTER sem; 
-	
-	pp->DetYearSemSave(year, sem);
-	cout << year << endl << sem << endl;      //debug_e
-
-	myfile <<
-		"Year1:\n"
-		"\tFall: " << pp->SaveStudyPlan(year, sem) << endl <<
-		"\tSpring: " << pp->SaveStudyPlan(year, sem) << endl <<
-		"\tSummer: " << pp->SaveStudyPlan(year, sem) << endl <<
-		"Year2:\n"
-		"\tFall: " << pp->SaveStudyPlan(year, sem) << endl <<
-		"\tSpring: " << pp->SaveStudyPlan(year, sem) << endl <<
-		"\tSummer: " << pp->SaveStudyPlan(year, sem) << endl <<
-		"Year3:\n"
-		"\tFall: " << pp->SaveStudyPlan(year, sem) << endl <<
-		"\tSpring: " << pp->SaveStudyPlan(year, sem) << endl <<
-		"\tSummer: " << pp->SaveStudyPlan(year, sem) << endl <<
-		"Year4:\n"
-		"\tFall: " << pp->SaveStudyPlan(year, sem) << endl <<
-		"\tSpring: " << pp->SaveStudyPlan(year, sem) << endl <<
-		"\tSummer: " << pp->SaveStudyPlan(year, sem) << endl <<
-		"Year5:\n"
-		"\tFall: " << pp->SaveStudyPlan(year, sem) << endl <<
-		"\tSpring: " << pp->SaveStudyPlan(year, sem) << endl <<
-		"\tSummer: " << pp->SaveStudyPlan(year, sem) << endl;
-
-=======
 	myfile.open("Saves\\" + filename + ".txt");
 	 vector<vector<vector<Course>>> ff = pp->ReturnALlCrs();
 	int n1=0;
@@ -91,7 +59,6 @@ bool ActionSave::Execute()
 	}
 	for (auto x : lines)
 		myfile<<x<<endl;
->>>>>>> origin/Phase1_kareem
 	myfile.close();
 	return true;
 }
